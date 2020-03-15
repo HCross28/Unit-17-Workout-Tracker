014e46738c90735e946c6dd54576c7da9f79fc11
HCross <haydenlcrossjunk@gmail.com>
2020-03-14 20:45:49 -0700

    adding busted code

---
+++ public/app.js
@@ -0,0 +1,86 @@
+const actionBtn = document.getElementById("action-button");
+
+const makeNote = document.getElementById("make-new");
+
+function getResults() {
+    //clearTodos();
+    fetch("/all")
+        .then(function(response) {
+            if (response.status !== 200) {
+                console.log("Looks like there was a problem. Status Code: " + response.status);
+                return;
+            }
+            response.json().then(function(data) {
+                newWorkout(data);
+            });
+        })
+        .catch(function(err) {
+            console.log("Fetch Error", err);
+        });
+}
+
+function newWorkout(res) {
+    for (var i = 0; i < res.length; i++) {
+        let data_id = res[i]["_id"];
+        let workoutType = res[i]["Workout-Type"];
+        let workoutList = document.getElementById("results");
+        snippet = `
+      <li class="data-entry">
+      <span class="dataTitle" data-id=${data_id}>${workoutType}</span>;
+      </li>`;
+        workoutList.insertAdjacentHTML("beforeend", snippet);
+    }
+}
+
+getResults();
+
+actionBtn.addEventListener("click", function(e) {
+    if (e.target.matches("#updater")) {
+        updateBtnEl = e.target;
+        data_id = updateBtnEl.getAttribute("data-id");
+        const title = document.getElementById("title").value;
+        const note = document.getElementById("note").value;
+        fetch("/update/" + data_id, {
+                method: "post",
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    title,
+                    note
+                })
+            })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                element.innerText = title
+                resetTitleAndNote();
+                let newButton = `<button id='make-new'>Submit</button>`;
+                actionBtn.innerHTML = newButton;
+                status.innerText = "Creating"
+            })
+            .catch(function(err) {
+                console.log("Fetch Error :-S", err);
+            });
+    } else if (e.target.matches("#make-new")) {
+        element = e.target;
+        data_id = element.getAttribute("data-id");
+        fetch("/submit", {
+                method: "post",
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    title: document.getElementById("title").value,
+                    note: document.getElementById("note").value,
+                    created: Date.now()
+                })
+            })
+            .then(res => res.json())
+            .then(res => newTodoSnippet([res]));
+        resetTitleAndNote();
+    }
+});
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const path = require("path");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "workout";
+const collections = ["workout"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+  });
+
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+
+    db.notes.insert(req.body, (error, data) => {
+    if (error) {
+        res.send(error);
+    } else {
+        res.send(data);
+    }
+    });
+});
+
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+  
+    db.notes.insert(req.body, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    });
+  });
+
+app.get("/all", (req, res) => {
+db.notes.find({}, (error, data) => {
+    if (error) {
+    res.send(error);
+    } else {
+    res.json(data);
+    }
+});
+});
+
+app.listen(3000, () => {
+console.log("App running on port 3000");
+});
